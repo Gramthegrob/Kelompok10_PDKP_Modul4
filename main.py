e88f8d336411ad6ee9e58f808f9b72d94b8fbb1e
CipluxX <126221755+CipluxX@users.noreply.github.com>
2023-04-03 00:31:12 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from function import *
+
+#memanggil function 
+panggil = metode (totalModal())
+print("total harga dengan diskon : " , diskon(total))
+
+
+panggil.trims()
+panggil.selesai(5)
+input("ENTER untuk selesai")
